261b6a4eb62367e73004ed3785b1a82d957005d4
Drew Pereli <drewpereli@gmail.com>
2020-05-30 21:37:55 -0700

    Slight style updates. Use momentjs to get humanized age text

--- app/components/post.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class PostComponent extends Component {
-  get ageText() {
-    let age = this.args.post.ageMinutes;
-    return `${age} minutes ago`;
-  }
-}
